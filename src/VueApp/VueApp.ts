d9c2c6796b14d174a56b978a608689abd7568fec
Пинчук Виталий А <Pinchuk_VA@pac.local>
2019-08-21 16:38:30 +0300

    removing not necessary vue double import (no default error)

--- src/VueApp/VueApp.ts
+++ src/VueApp/VueApp.ts
@@ -1,11 +1,6 @@
-// @link: https://github.com/vuejs/vue/issues/6815#issuecomment-336721984
-/* eslint-disable import/no-duplicates */
-import VueD from 'vue';
-import * as VueN from 'vue';
+import Vue from 'vue';
 
 import { components, selector as el } from './VueApp.conf';
-
-const Vue = VueD || VueN;
 
 export default class VueApp {
 	public static init(): void {
--- src/common.blocks/IamHeader/IamHeader.ts
+++ src/common.blocks/IamHeader/IamHeader.ts
@@ -1,11 +1,6 @@
-// @link: https://github.com/vuejs/vue/issues/6815#issuecomment-336721984
-/* eslint-disable import/no-duplicates */
-import VueD from 'vue';
-import * as VueN from 'vue';
+import Vue from 'vue';
 
 import { name, template } from './IamHeader.conf';
-
-const Vue = VueD || VueN;
 
 const IamHeader = Vue.extend({
 	name,
