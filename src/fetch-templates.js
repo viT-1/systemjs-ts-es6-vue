60e4f6df7919829e2e7424597eb3a25165558687
Пинчук Виталий А <Pinchuk_VA@pac.local>
2019-09-27 17:51:48 +0300

    unactual file removed

--- src/fetch-templates.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-fetch('./templates.htm')
-	.then(function (resp) { return resp.text(); })
-	.then(function (data) {
-		return document.querySelector('#js-fetch-templates').outerHTML = data;
-	});
